5af5a53560ba4626fbb2337d04d3a18988105ce0
ianmacdougald <35744006+ianmacdougald@users.noreply.github.com>
2023-11-01 15:59:52 -0400

    Transform HomePage component to App

---
+++ client/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from './components/NavBar';
+import MainHeader from './components/MainHeader';
+import Features from './components/Features';
+import Press from './components/Press';
+import Contributor from './components/Contributor';
+import Particles from './components/Particles';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Navbar />
+      <MainHeader />
+      <Features />
+      <Press />
+      <Contributor />
+      <Particles />
+    </div>
+  );
+};
+
+export default App;
