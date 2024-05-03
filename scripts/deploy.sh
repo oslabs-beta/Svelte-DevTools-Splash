85c0ff4ff186543ecd2f5204a6992a9aea80df3d
Alex Vranas <avranas42@gmail.com>
2024-05-03 16:51:08 -0700

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
     echo "Processing deploy.sh"
     # Set EB BUCKET as env variable
-    EB_BUCKET=[S3 BUCKET NAME]
+    EB_BUCKET=elasticbeanstalk-us-east-1-294110367996
     # Set the default region for aws cli
     aws configure set default.region us-east-1
     # Log in to ECR
@@ -8,9 +8,9 @@
     # Build docker image based on our production Dockerfile
     docker build -t vranasware/splash-prod .
     # tag the image with the GitHub SHA
-    docker tag vranasware/splash-prod:latest [ECR URI]:$GITHUB_SHA
+    docker tag vranasware/splash-prod:latest public.ecr.aws/w2g8f3y2/splash:$GITHUB_SHA
     # Push built image to ECS
-    docker push [ECR URI]:$GITHUB_SHA
+    docker push public.ecr.aws/w2g8f3y2/splash:$GITHUB_SHA
     # Use the linux sed command to replace the text '<VERSION>' in our Dockerrun file with the GitHub SHA key
     sed -i='' "s/<VERSION>/$GITHUB_SHA/" Dockerrun.aws.json
     # Zip up our codebase, along with modified Dockerrun and our .ebextensions directory
@@ -18,6 +18,6 @@
     # Upload zip file to s3 bucket
     aws s3 cp mm-prod-deploy.zip s3://$EB_BUCKET/mm-prod-deploy.zip
     # Create a new application version with new Dockerrun
-    aws elasticbeanstalk create-application-version --application-name [your eb application name] --version-label $GITHUB_SHA --source-bundle S3Bucket=$EB_BUCKET,S3Key=mm-prod-deploy.zip
+    aws elasticbeanstalk create-application-version --application-name Svelte Devtools+ --version-label $GITHUB_SHA --source-bundle S3Bucket=$EB_BUCKET,S3Key=mm-prod-deploy.zip
     # Update environment to use new version number
-    aws elasticbeanstalk update-environment --environment-name [your eb environment name] --version-label $GITHUB_SHA
+    aws elasticbeanstalk update-environment --environment-name SvelteDevtools-env  --version-label $GITHUB_SHA
