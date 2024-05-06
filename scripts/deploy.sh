77ffb0fe3bfda8ab6f07e8619690e6e08076abc9
Alex Vranas <avranas42@gmail.com>
2024-05-06 15:58:24 -0700

    Add quotes around application name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,6 +18,6 @@
     # Upload zip file to s3 bucket
     aws s3 cp mm-prod-deploy.zip s3://$EB_BUCKET/mm-prod-deploy.zip
     # Create a new application version with new Dockerrun
-    aws elasticbeanstalk create-application-version --application-name Svelte Devtools+ --version-label $GITHUB_SHA --source-bundle S3Bucket=$EB_BUCKET,S3Key=mm-prod-deploy.zip
+    aws elasticbeanstalk create-application-version --application-name "Svelte Devtools+" --version-label $GITHUB_SHA --source-bundle S3Bucket=$EB_BUCKET,S3Key=mm-prod-deploy.zip
     # Update environment to use new version number
     aws elasticbeanstalk update-environment --environment-name SvelteDevtools-env  --version-label $GITHUB_SHA
